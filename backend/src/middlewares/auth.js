e0f3938bd9d76d0c39221412abd5e33219429b38
ivisheshrajput <ivisheshrajput@gmail.com>
2025-04-23 05:21:34 +0530

    Writing auth Middleware

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,21 @@
+const adminAuth = (req, res, next) => {
+  const token = "xyz"; //change this to check other routes for authentication
+  const isAuthenticated = token === "xyz";
+  if (!isAuthenticated) {
+    res.status(401).send("Admin in NOT Authenticated");
+  } else {
+    next();
+  }
+};
+
+const userAuth = (req, res, next) => {
+  const token = "xyz";
+  const isAuthenticated = token === "xyz";
+  if (!isAuthenticated) {
+    res.send("User in NOT Authenticated");
+  } else {
+    next();
+  }
+};
+
+module.exports = { adminAuth, userAuth };
