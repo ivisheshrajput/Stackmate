e3ca99fd2283a3674eebd7ece773fbf81ce3f138
ivisheshrajput <ivisheshrajput@gmail.com>
2025-04-25 06:28:05 +0530

    Get Api calls

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,33 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  const emailUser = req.body.emailt;
+  try {
+    const user = await Users.find({ email: emailUser });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).res("Something went wrong:" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await Users.find({});
+    if (!users) {
+      res.send("No user is present");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something is wrong");
+  }
+});
+
 app.use("/", (req, res) => {
   res.send("Hello to Dashboard");
 });
